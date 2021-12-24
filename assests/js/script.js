e9c10bb938248aded014564a60e2eb91ddd27618
Jadeja Devdeepsinh Ahoksinh <81241950+Devdeepsinh0591@users.noreply.github.com>
2021-12-24 11:53:33 +0530

    Update script.js

--- assests/js/script.js
+++ assests/js/script.js
@@ -102,7 +102,7 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
   (function(){
   var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
   s1.async=true;
-  s1.src='https://embed.tawk.to/60f70460649e0a0a5ccd22a7/1fb2ei71o';
+  s1.src='https://embed.tawk.to/61c5668ec82c976b71c34d09/1fnlhh7p9';
   s1.charset='UTF-8';
   s1.setAttribute('crossorigin','*');
   s0.parentNode.insertBefore(s1,s0);
