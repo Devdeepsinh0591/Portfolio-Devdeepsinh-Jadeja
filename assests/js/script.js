45d3eb3d428b15a7f6897a0e5f3c5f84a9044850
Jadeja Devdeepsinh Ahoksinh <81241950+Devdeepsinh0591@users.noreply.github.com>
2022-02-10 13:12:10 +0530

    Update script.js

--- assests/js/script.js
+++ assests/js/script.js
@@ -78,6 +78,29 @@ function fadeOut(){
 window.onload = fadeOut;
 // pre loader end
 
+
+//enable developer mode 
+
+document.onkeydown = function(e) {
+  if(e.keyCode == 123) {
+     return true;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return true;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return true;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return true;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return true;
+  }
+}
+
+
+
 // Start of Tawk.to Live Chat
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
   (function(){
