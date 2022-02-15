a458735a55665b1e36e058ac49e8be564c1c277f
Jadeja Devdeepsinh Ahoksinh <81241950+Devdeepsinh0591@users.noreply.github.com>
2022-02-15 20:12:30 +0530

    Update script.js

--- assests/js/script.js
+++ assests/js/script.js
@@ -82,19 +82,19 @@ window.onload = fadeOut;
 // disable developer mode
 document.onkeydown = function(e) {
 if(e.keyCode == 123) {
-return false;
+return true;
 }
 if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-return false;
+return true;
 }
 if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-return false;
+return true;
 }
 if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-return false;
+return true;
 }
 if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-return false;
+return true;
 }
 }
 
